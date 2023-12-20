7412c548d8291c4754a206e66a6d3e764cd043a5
Richin Rajeev T R <richinrajeev796@gmail.com>
2023-12-20 19:35:34 +0530

    multer err fixed

--- server/controller/adminController/multer.js
+++ server/controller/adminController/multer.js
@@ -1,17 +1,17 @@
 const Multer = require('multer');
 const path = require('path');
-
 const storage = Multer.diskStorage({
     destination: (req, file, callBack) => {
-        const assetsDir = path.join(__dirname,'../../../','assets');
-        const destinationPath = path.join(assetsDir,'uploadedImages');
+        const assetsDir = path.join(__dirname,'../../../assets');
+        const destinationPath = path.join(assetsDir,'uploads');
         callBack(null, destinationPath);
     },
     filename: (req, file, callBack) => {
+        console.log(file);
           const extenction = file.originalname.substring(file.originalname.lastIndexOf('.'));
-          callBack(null, `${file.fieldname}-${Date.now()}${extenction}`);  
+          callBack(null, `${file.originalname}-${Date.now()}${extenction}`);  
     }
 });
 
 
-module.exports = store = Multer({storage});
+exports.store = Multer({storage});
